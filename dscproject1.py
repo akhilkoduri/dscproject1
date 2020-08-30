7f60e93a659cf060aa58a44e57c75319c62d2b45
Akhil Koduri <65801880+akhilkoduri@users.noreply.github.com>
2020-08-30 16:03:23 -0400

    Update dscproject1.py

--- dscproject1.py
+++ dscproject1.py
@@ -4,7 +4,7 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+/*This notebook is solely for data science capstone project in IBM Data Science certification */
 #Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,3 +12,4 @@ import pandas as pd
 
 A= 'Hello Capstone Project Course!'
 print(A)
+
