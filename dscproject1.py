a259453753cfe7e4ed7860f60619a209f127aee0
Akhil Koduri <65801880+akhilkoduri@users.noreply.github.com>
2020-08-30 15:58:19 -0400

    Add files via upload

---
+++ dscproject1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+A= 'Hello Capstone Project Course!'
+print(A)
